093b946899134170a94541c52cfd18357ccce7df
milanj <milence@gmail.com>
2015-07-23 23:53:08 +0200

    bnc

---
+++ ip_refresh/request.php
@@ -0,0 +1,34 @@
+<?php
+session_start();
+
+require("../../site_includes/php/constants.php");
+
+require("../../site_includes/php/log_to_database.php");
+
+//SQLinject zastita
+$HS_NAME = str_replace("'","",$_REQUEST['data']);
+$HS_NAME = str_replace("\"","",$HS_NAME);
+$HS_NAME = str_replace("(","",$HS_NAME);
+$HS_NAME = str_replace(")","",$HS_NAME);
+
+$HS_PORT = intval ($_REQUEST['p']);
+
+$qry = "SELECT IP FROM ip_refresh WHERE hs_name = '".$HS_NAME."'";
+$res = mysql_query($qry);
+
+if(mysql_num_rows($res) == 0){
+	$qry1 = "INSERT INTO ip_refresh (IP,port,updated,hs_name) VALUES ('".$_SERVER['REMOTE_ADDR']."','".$HS_PORT."',NOW(),'".$HS_NAME."')";
+	if(!mysql_query($qry1)){
+		echo "false";
+	}else{
+		echo "true";
+	}
+}else{
+	$qry2 = "UPDATE ip_refresh SET IP = '".$_SERVER['REMOTE_ADDR']."', port = '".$HS_PORT."', updated = NOW() WHERE hs_name = '".$HS_NAME."'";
+	if(!mysql_query($qry2)){
+		echo "false";
+	}else{
+		echo "true";
+	}	
+}
+?>
